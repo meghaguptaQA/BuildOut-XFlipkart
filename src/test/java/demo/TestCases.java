578f99ab8d453d20e81b6d9329423abf96373924
Megha Gupta <meghagupta22@gmail.com>
2025-02-17 22:26:27 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,42 +1,47 @@
 package demo;
-
+import demo.wrappers.Wrappers;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
+//import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
+import org.testng.annotations.Parameters;
 
+//import com.beust.jcommander.Parameters;
+
+import java.sql.Wrapper;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
-
-
 // import io.github.bonigarcia.wdm.WebDriverManager;
-import demo.wrappers.Wrappers;
+
 
 public class TestCases {
     ChromeDriver driver;
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
-
-     
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
     @BeforeTest
     public void startBrowser()
     {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
@@ -50,7 +55,7 @@ public class TestCases {
 
         driver = new ChromeDriver(options);
 
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
     }
 
     @AfterTest
@@ -59,5 +64,156 @@ public class TestCases {
         driver.close();
         driver.quit();
 
+    }
+    @Test
+    @Parameters({ "ProductName1" })
+    public void testCase01(@Optional("testUser") String ProductName1) throws InterruptedException 
+     {
+        System.out.println("Start Test case: testCase01");
+        //Open the URL to Flipkart home page
+        Wrappers.openURL(driver,"https://www.flipkart.com/");
+        //String url=Wrappers.GetCurrentURL(driver);
+        Thread.sleep(3000);
+        //Search for product Washing Machine
+        Wrappers.SearchForProduct(driver, ProductName1);
+        Thread.sleep(3000);
+        //Sort the search results by populartity
+        Duration timeout=Duration.ofMillis(10000);
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Popularity']")));
+        WebElement btnSortByPopularity=driver.findElement(By.xpath("//div[text()='Popularity']"));
+        Wrappers.btn_click(btnSortByPopularity);
+        Thread.sleep(5000);
+
+        //Get the search results with rating 4 or less
+        List<WebElement> SearchRes=driver.findElements(By.xpath("//span[contains(@id,'productRating')]/div"));
+        int count=0;
+        for(WebElement res:SearchRes)
+        {   Float i=Float.parseFloat(res.getText().toString());
+            System.out.println(i);
+             if (i<=4.0) 
+             {
+                count++;
+             }
+        }
+        System.out.println("Count of items with rating less than or equal to 4 stars is: "+count);
+        SearchRes=driver.findElements(By.xpath("//span[contains(@id,'productRating')]/div[text()<='4']"));
+        System.out.println("Count of items with rating less than or equal to 4 stars is: "+SearchRes.size());                
+        System.out.println("end Test case: testCase01");
+        
+    }
+   
+    @Test
+    @Parameters({ "ProductName2" })
+    public void testCase02(@Optional("testUser") String ProductName2) throws InterruptedException 
+     {
+        System.out.println("Start Test case: testCase02");
+        //Open the URL to Flipkart home page
+        Wrappers.openURL(driver,"https://www.flipkart.com/");
+        //String url=Wrappers.GetCurrentURL(driver);
+        Thread.sleep(3000);
+        //Search for product IPhone
+        Wrappers.SearchForProduct(driver, ProductName2);
+        Thread.sleep(3000);
+
+        //Get the search results with discount% greater than 17
+        List<WebElement> SearchRes=driver.findElements(By.xpath("//div[@class='yKfJKb row']"));
+        int count=0;
+        for(WebElement res:SearchRes)
+        {   //div[@class='UkUFwK']//span     
+            WebElement discnt =res.findElement(By.xpath("./div[2]//span"))  ;    
+            String i=discnt.getText().toString();
+           // System.out.println("I is:"+i);
+            String[] arrStr=i.split("%");
+            Integer discount=Integer.parseInt(arrStr[0]);
+            //System.out.println("Discount is: "+discount);
+             if (discount>17) 
+             {
+
+                count++;
+                WebElement title=res.findElement(By.xpath(".//div[@class='KzDlHZ']"));
+                System.out.println(title.getText().toString()+" : Discount is - "+discount+"%");
+             }
+        }
+        if(count==0)
+        {
+            System.out.println("No Iphone with more than 17% discount.");
+        }
+                     
+        System.out.println("end Test case: testCase02");
+        
+    }
+    @Test
+    @Parameters({ "ProductName3" })
+    public void testCase03(@Optional("testUser") String ProductName3) throws InterruptedException 
+     {
+        System.out.println("Start Test case: testCase03");
+        //Open the URL to Flipkart home page
+        Wrappers.openURL(driver,"https://www.flipkart.com/");
+        //String url=Wrappers.GetCurrentURL(driver);
+        Thread.sleep(3000);
+        //Search for product Coffee Mug
+        Wrappers.SearchForProduct(driver, ProductName3);
+        Thread.sleep(3000);
+        //Click on checkbox 4* and ab0ve
+        String txt4starabove="4";
+        //div[text()='4★ & above']//preceding-sibling::div
+        
+        WebElement checkbox4Star=driver.findElement(By.xpath("//div[contains(text(),'"+txt4starabove+"') and contains(text(),'above')]//preceding-sibling::div"));
+        Wrappers.btn_click(checkbox4Star);
+        Thread.sleep(9000);
+        //Get the search results with discount% greater than 17
+        List<WebElement> SearchRes=driver.findElements(By.xpath("//div[@class='slAVV4']//div/span[2]"));
+       // int count=SearchRes.size();
+       // System.out.println("");
+        //System.out.println("Count is :"+count);
+        //System.out.println("");
+        HashMap<Integer,String> map=new HashMap<>();
+        //count=0;
+        for(WebElement res:SearchRes)
+        {   
+            //Thread.sleep(2000);
+            String i=res.getText().toString();
+            WebElement Url=Wrappers.FindElement(res, "./ancestor::div[@class='slAVV4']//img[@class='DByuf4']");
+            WebElement title=Wrappers.FindElement(res, "./ancestor::div[@class='slAVV4']//a[2]");
+            String URL=Url.getAttribute("src").toString();
+            String Title=title.getAttribute("title").toString();
+             //System.out.println("I is:"+i);
+             //i=i.replaceAll([^0-9],"");
+             i=i.replace("(", "");
+             i=i.replace(")", "");
+             i=i.replace(",", "");
+             Integer reviews=Integer.parseInt(i);
+             //System.out.println("no of reviews are "+reviews);
+             
+             //Thread.sleep(2000);
+             
+             //System.out.println("URL is "+URL);
+             
+             //System.out.println("Title is "+Title);
+             map.put(reviews, "Title: "+Title+", Image URL: "+URL);
+             //count++;
+             //System.out.println("Count increment :"+count);
+        }
+        ArrayList<Integer> sortedKeys  = new ArrayList<Integer>(map.keySet());
+            //      Collections.sort(userReviewCountList,Collections.reverseOrder());   [^\\d]
+        //Collections.sort(sortedKeys,Collections.reverseOrder());
+        //Collections.reverseOrder(sortedKeys);
+        Collections.sort(sortedKeys);
+        int size=sortedKeys.size();
+        System.out.println("Title and Image URL for top 5 reviews are :");
+        for(int l=size-1;l>(size-6);l--)
+        {
+            //System.out.println("Key is: "+sortedKeys.get(l));
+            System.out.println("Reviews:"+sortedKeys.get(l)+"  ,  "+map.get(sortedKeys.get(l)));
+        }
+        // for(Map.Entry<Integer,String> entry : map.entrySet())
+        // {
+        //         System.out.println("KEY :"+entry.getKey()+"  Title and URL :"+entry.getValue());
+                
+        // }
+                     
+        System.out.println("end Test case: testCase03");
+        
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,22 +1,96 @@
 package demo.wrappers;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import demo.TestCases;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Wrappers {
+
     /*
      * Write your selenium wrappers here
      */
+    public static void openURL(WebDriver driver,String browserURL) {
+
+		System.out.println("Maximize the window");
+		driver.manage().window().maximize();
+
+		System.out.println("Opening Website -->" + browserURL);
+		driver.get(browserURL);
+	}
+    public static String GetCurrentURL(WebDriver driver)
+    {   
+        try {       
+             String url=driver.getCurrentUrl();
+             return url;
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+                return null;
+
+    }
+    public static void enter_Value(WebElement element,String attributeValue)
+    {
+        try 
+        {
+            element.clear();
+            element.sendKeys(attributeValue);
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public static void btn_click(WebElement element)
+    {
+        try 
+        {
+            element.click();
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+
+    }
+    public static WebElement FindElement(WebElement res,String xpath)
+    {
+        try 
+        {
+            WebElement element=res.findElement(By.xpath(xpath));
+            return element;
+        } catch (Exception e) 
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+    public static String date_decrmenter(LocalDate todaysdate,Integer days )
+    {
+        
+        LocalDate date7daysbefore=todaysdate.minusDays(7);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/YYYY");
+        String datetoenter = date7daysbefore.format(format);
+        return datetoenter;
+    }
+    public static void SearchForProduct(WebDriver driver,String product)
+    {
+        WebElement txtboxSearch=driver.findElement(By.xpath("//input[@title='Search for Products, Brands and More']"));
+        Wrappers.enter_Value(txtboxSearch,product+Keys.ENTER);
+
+    }
+
 }
